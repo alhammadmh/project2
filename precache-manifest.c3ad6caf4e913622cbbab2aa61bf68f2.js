44ab513e3bdca0af7f9abb68641dfe55ca76b42a
mohammed <alhammad2013@gmail.com>
2019-04-03 16:17:38 +0300

    Updates

---
+++ precache-manifest.c3ad6caf4e913622cbbab2aa61bf68f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7de6857c9e5f2e8fb148",
+    "url": "/project2/static/js/runtime~main.ab33e848.js"
+  },
+  {
+    "revision": "e3f2c13b3352cd960f4e",
+    "url": "/project2/static/js/main.b344c18e.chunk.js"
+  },
+  {
+    "revision": "9b93c46c98223a6df09f",
+    "url": "/project2/static/js/2.c80e74eb.chunk.js"
+  },
+  {
+    "revision": "e3f2c13b3352cd960f4e",
+    "url": "/project2/static/css/main.a9068486.chunk.css"
+  },
+  {
+    "revision": "017aa110363037b5726ebe8e442ba3c8",
+    "url": "/project2/index.html"
+  }
+];
